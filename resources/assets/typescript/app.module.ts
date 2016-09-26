0de78986edd721960e2e760fddb96b7ccf39c86b
Wudtichai Chumsangchotsagun <wudtichai@thinknet.co.th>
2016-09-26 21:01:07 +0700

    complete

--- resources/assets/typescript/app.module.ts
+++ resources/assets/typescript/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { HeaderAppComponent } from './header-app.component';
 import { ImageSliderComponent } from './image-slider.component';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ HeaderAppComponent, ImageSliderComponent, AppComponent ],
+  declarations: [ 
+    HeaderAppComponent, 
+    ImageSliderComponent, 
+    LoginComponent, 
+    AppComponent 
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ resources/assets/typescript/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'app/login.component.html',
+  styleUrls: ['app/login.component.css']
+})
+export class LoginComponent { }
