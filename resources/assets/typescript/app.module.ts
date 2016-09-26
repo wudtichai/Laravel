641d03a9e54245532b1eb9585790b6aeffe3cba6
Wudtichai Chumsangchotsagun <wudtichai@thinknet.co.th>
2016-09-26 18:24:07 +0700

    slide

--- resources/assets/typescript/app.module.ts
+++ resources/assets/typescript/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { HeaderAppComponent } from './header-app.component';
 import { ImageSliderComponent } from './image-slider.component';
-import { Image } from './image';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ HeaderAppComponent, Image, ImageSliderComponent, AppComponent ],
+  declarations: [ HeaderAppComponent, ImageSliderComponent, AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- resources/assets/typescript/image-slider.component.ts
+++ resources/assets/typescript/image-slider.component.ts
@@ -7,17 +7,38 @@ import { Image } from './image';
   styleUrls: ['app/image-slider.component.css']
 })
 export class ImageSliderComponent {
-
+  
+  current: number;
+  count: number;
   images: Image[] = [
     {"src": "images/img1.jpg", show:true},
     {"src": "images/img2.jpg", show:false},
     {"src": "images/img3.jpg", show:false}
   ];
 
-  showSlides(image) {
-    for (let img of this.images) {
-      img.show = false;
-    }
-    image.show = true;
+  constructor() {
+    this.current = 0;
+    this.count = this.images.length;
   }
+
+  ngOnInit() {
+    setInterval(() => this.plusSlides(1), 8000);
+  }
+
+  showSlide(n) {
+    this.images[this.current].show = false;
+    this.images[n].show = true;
+    this.current = n;
+  }
+
+  plusSlides(n){
+    let current = this.mod((this.current + n),this.count);
+    console.log(current);
+    this.showSlide(current); 
+  }
+
+  mod(n, m) {
+    return ((n % m) + m) % m;
+  }
+
 }
