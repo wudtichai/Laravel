8feed41dcfbd7cafc884ded2b6e59811c8b9a078
Wutthichai Chumsaengchotsakul <wudtichai_37500@hotmail.com>
2016-10-08 14:26:54 +0700

    complete

--- resources/assets/typescript/learn/end.component.ts
+++ resources/assets/typescript/learn/end.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './../auth.service'
+import { Router } from '@angular/router';
 
 @Component({
-  template: 'End'
+  templateUrl: 'app/learn/end.component.html',
+  styleUrls: ['app/learn/end.component.css']
 })
-export class EndComponent { }
+export class EndComponent { 
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+        this.router.navigate(['/login']);
+    });
+  }
+
+}
--- resources/views/angular2.blade.php
+++ resources/views/angular2.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Kanit|Maitree&subset=thai" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kanit|Trirong&subset=thai" rel="stylesheet">
     <link rel="stylesheet" href="css/app.css">
 
     <!-- 1. Load libraries -->
