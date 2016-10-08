0abb11504e6009777bb6f88bc69565792e64e642
Wutthichai Chumsaengchotsakul <wudtichai_37500@hotmail.com>
2016-10-08 15:45:30 +0700

    ok

--- resources/assets/typescript/learn/nature.component.ts
+++ resources/assets/typescript/learn/nature.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { QUESTION_LIST } from './question-list';
 import { NatureService }    from './nature.service';
+import { AuthService }    from './../auth.service';
 import { Router }      from '@angular/router';
 
 @Component({
@@ -15,7 +16,7 @@ export class NatureComponent {
 
   private answer;
 
-  constructor(private natureService: NatureService, public router: Router) { }
+  constructor(private natureService: NatureService, private authService: AuthService, public router: Router) { }
 
   ngOnInit() {
     this.resetAnswer();
@@ -32,8 +33,9 @@ export class NatureComponent {
 
   onSubmit() {
     this.natureService.send(this.answer).subscribe(
-      () => {
+      (res) => {
         this.resetAnswer();
+        this.authService.updateUser(res.data.user);
         this.router.navigate(['/learn/lesson', 1]);
       }
     );
